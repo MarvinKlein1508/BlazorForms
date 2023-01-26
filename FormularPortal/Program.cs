76131adef46b8dd000e62349a7b73af2129c5431
Marvin Klein <marvin.klein@harold-scholz.de>
2023-01-26 15:11:29 +0100

    Projektdateien hinzufügen.

---
+++ FormularPortal/Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Plk.Blazor.DragDrop;
+using System.Reflection;
+
+namespace FormularPortal
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+            builder.Services.AddServerSideBlazor();
+            builder.Services.AddBlazorDragDrop();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.MapBlazorHub();
+            app.MapFallbackToPage("/_Host");
+
+            app.Run();
+        }
+
+        public static string GetVersion()
+        {
+            return Assembly.GetEntryAssembly()!.GetName()!.Version!.ToString(3);
+        }
+    }
+}
---
+++ FormularPortal/wwwroot/assets/js/blazor-scripts.js
@@ -0,0 +1,88 @@
+﻿window.blazorHelpers = {
+    startUp: () => {
+        /**
+         * Create Sidebar Wrapper
+         */
+        let sidebarEl = document.getElementById("sidebar")
+
+
+        // Fix mobile Menu for blazor PWA behaviour
+        var elements = document.getElementsByClassName("blazor-link");
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].addEventListener('click', function () {
+                var w = window.innerWidth;
+                if (w <= 1024) {
+                    sidebarEl.classList.remove("active");
+                }
+            });
+        }
+
+        // Disable tap zoom on mobile devices
+        var lastTouchEnd = 0;
+        document.addEventListener('touchend', function (event) {
+            var now = (new Date()).getTime();
+            if (now - lastTouchEnd <= 300) {
+                event.preventDefault();
+            }
+            lastTouchEnd = now;
+        }, false);
+    },
+    showSweetAlert: (icon, title, html, timer) => {
+        Swal.fire({
+            icon: icon,
+            title: title,
+            html: html,
+            timer: timer
+        });
+    },
+    showToast: (type, message, timeout = 5000) => {
+        toastr.options = {
+            "closeButton": false,
+            "debug": false,
+            "newestOnTop": true,
+            "progressBar": true,
+            "positionClass": "toast-bottom-right",
+            "preventDuplicates": false,
+            "onclick": null,
+            "showDuration": "300",
+            "hideDuration": "1000",
+            "timeOut": timeout,
+            "extendedTimeOut": "1000",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "fadeIn",
+            "hideMethod": "fadeOut"
+        };
+
+
+        switch (type) {
+            case "warning":
+                toastr.warning(message);
+                break;
+            case "error":
+                toastr.error(message);
+                break;
+            default:
+                toastr.success(message);
+                break;
+        }
+    },
+    setFocusById: (elementId) => {
+        var element = document.getElementById(elementId);
+        if (element) {
+            element.focus();
+        }
+    },
+    open: (url) => {
+        window.open(url, '_blank');
+    },
+    isDesktop: () => {
+        var w = window.innerWidth;
+        return w > 1024;
+    }
+}
+
+
+
+
+
