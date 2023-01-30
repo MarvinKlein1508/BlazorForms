6fb639168281bf2ed7da3da6220fd26714599281
Marvin Klein <marvin.klein@harold-scholz.de>
2023-01-30 10:47:13 +0100

    Rename to FormEditor

---
+++ FormularPortal.Core/Models/Form.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormularPortal.Core.Models
+{
+    public class Form
+    {
+        public int FormId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public bool IsOnlyAvailableForLoggedInUsers { get; set; }
+        public bool IsActive { get; set; }
+        public List<FormRow> Rows { get; set; } = new();
+    }
+}
--- FormularPortal.Core/Services/IModelService.cs
+++ FormularPortal.Core/Services/IModelService.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FormularPortal.Core.Models
+namespace FormularPortal.Core.Services
 {
-    public class Formular
+    public interface IModelService
     {
     }
 }
