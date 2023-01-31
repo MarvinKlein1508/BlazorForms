4b0ad12cd35e66d7280141c0a41c226b61ee4259
Marvin Klein <marvin.klein@harold-scholz.de>
2023-01-31 12:27:45 +0100

    Add filters

---
+++ FormularPortal.Core/Filters/FormFilter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormularPortal.Core.Filters
+{
+    public class FormFilter : PageFilterBase
+    {
+        public bool ShowOnlyActiveForms { get; set; }
+        public bool ShowOnlyFormsWhichRequireLogin { get; set; }
+    }
+}
