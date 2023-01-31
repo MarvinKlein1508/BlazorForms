bab65de4172b016770eceb8f197fc6afcdf0cf1e
Marvin Klein <marvin.klein@harold-scholz.de>
2023-01-31 12:27:56 +0100

    Add filters

--- FormularPortal.Core/Filters/PageFilterBase.cs
+++ FormularPortal.Core/Filters/PageFilterBase.cs
@@ -1,11 +1,11 @@
 ﻿namespace FormularPortal.Core.Filters
 {
-    public abstract class PageFilter : FilterBase
+    public abstract class PageFilterBase : FilterBase
     {
         private int _pageNumber = 1;
         private int _limit = 30;
 
         public int PageNumber { get => _pageNumber; set => _pageNumber = value < 1 ? 1 : value; }
-        public int Limit { get => _limit; set => _limit = value < 1 ? 1: value; }
+        public int Limit { get => _limit; set => _limit = value < 1 ? 1 : value; }
     }
 }
