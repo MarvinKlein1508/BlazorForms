108c8955ea6471455d388d7a4b8a53ba3b575394
Marvin Klein <marvin.klein@harold-scholz.de>
2023-02-09 08:55:52 +0100

    Add CompareField

--- FormularPortal.Core/Models/RuleSet.cs
+++ FormularPortal.Core/Models/RuleSet.cs
@@ -1,21 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DatabaseControllerProvider;
 
 namespace FormularPortal.Core.Models
 {
-    public class RuleSet
+    public class RuleSet : IDbModel
     {
+        [CompareField("rule_id")]
+        public int RuleId { get; set; }
+        [CompareField("logical_operator")]
         public LogicalOperator LogicalOperator { get; set; }
+        [CompareField("element_guid")]
         public Guid ElementGuid { get; set; }
-        public FormElement? Element { get; set; }
-        public ComparisonOperator CompareOperator { get; set; }
+        [CompareField("comparison_operator")]
+        public ComparisonOperator ComparisonOperator { get; set; }
+        [CompareField("value_boolean")]
         public bool ValueBoolean { get; set; }
+        [CompareField("value_string")]
         public string ValueString { get; set; } = string.Empty;
+        [CompareField("value_number")]
         public decimal ValueNumber { get; set; }
+        [CompareField("value_date")]
         public DateTime ValueDate { get; set; }
+        [CompareField("sort_order")]
+        public int SortOrder { get; set; }
+
+
+
+        public int Id => RuleId;
+        public FormElement? Element { get; set; }
+
+        public Dictionary<string, object?> GetParameters()
+        {
+            return new Dictionary<string, object?>
+            {
+                { "RULE_ID", RuleId },
+                { "LOGICAL_OPERATOR", LogicalOperator.ToString() },
+                { "ELEMENT_GUID", ElementGuid.ToString() },
+                { "COMPARISON_OPERATOR", ComparisonOperator.ToString() },
+                { "VALUE_BOOLEAN", ValueBoolean },
+                { "VALUE_STRING", ValueString },
+                { "VALUE_NUMBER", ValueNumber },
+                { "VALUE_DATE", ValueDate },
+                { "SORT_ORDER", SortOrder}
+            };
+        }
     }
 
     public enum LogicalOperator
