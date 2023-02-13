4f2a7ca79ffe44a2e7b62d293f5f76d7ad7570c6
Marvin Klein <marvin.klein@harold-scholz.de>
2023-02-13 09:42:03 +0100

    Validate Name of form

---
+++ FormularPortal.Core/Validators/Admin/FormRowValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using FormularPortal.Core.Models;
+
+namespace FormularPortal.Core.Validators.Admin
+{
+    public class FormRowValidator : AbstractValidator<FormRow>
+    {
+        public FormRowValidator(IValidator<FormColumn> columnValidator)
+        {
+            RuleForEach(x => x.Columns) 
+                .SetValidator(columnValidator);
+        }
+    }
+}
