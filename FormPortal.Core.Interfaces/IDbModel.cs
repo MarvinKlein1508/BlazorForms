a6ac5bd2768ae29e79fdd18fdf8b6d55f29c6432
Marvin Klein <marvin.klein@harold-scholz.de>
2023-02-15 14:07:01 +0100

    Add code documentation

--- FormPortal.Core.Interfaces/IDbModel.cs
+++ FormPortal.Core.Interfaces/IDbModel.cs
@@ -2,7 +2,14 @@
 {
     public interface IDbModel
     {
+        /// <summary>
+        /// Gets the unique database identifier for the object.
+        /// </summary>
         int Id { get; }
+        /// <summary>
+        /// Gets a dictionary of parameters for the object which can be used in Dapper-Queries.
+        /// </summary>
+        /// <returns></returns>
         Dictionary<string, object?> GetParameters();
     }
 }
