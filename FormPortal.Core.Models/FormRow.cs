9699ec6b5d91ad5eb229891ef24d7574675f22ca
Marvin Klein <marvin.klein@harold-scholz.de>
2023-02-17 09:19:09 +0100

    Add IsVisible

--- FormPortal.Core.Models/FormRow.cs
+++ FormPortal.Core.Models/FormRow.cs
@@ -2,6 +2,7 @@
 using FormPortal.Core.Constants;
 using FormPortal.Core.Interfaces;
 using FormPortal.Core.Models.FormElements;
+using Org.BouncyCastle.Asn1.Esf;
 
 namespace FormPortal.Core.Models
 {
@@ -97,5 +98,114 @@ namespace FormPortal.Core.Models
 
             return "col";
         }
+
+        public bool IsVisible()
+        {
+            if (RuleType is not RuleType.Visible)
+            {
+                return true;
+            }
+
+            if (!Rules.Any())
+            {
+                return true;
+            }
+
+            // Check if all rules are true
+            return ValidateRules();
+        }
+
+        private bool ValidateRules()
+        {
+            if (!Rules.Any())
+            {
+                return true;
+            }
+
+
+            List<(LogicalOperator logicOperator, bool result)> checkedRules = new();
+         
+            foreach (var rule in Rules)
+            {
+                var element = rule.Element;
+                if (element is FormNumberElement numberElement)
+                {
+                    bool result = rule.ComparisonOperator switch
+                    {
+                        ComparisonOperator.Equals => numberElement.Value == rule.ValueNumber,
+                        ComparisonOperator.NotEquals => numberElement.Value != rule.ValueNumber,
+                        ComparisonOperator.GreaterThan => numberElement.Value > rule.ValueNumber,
+                        ComparisonOperator.LessThan => numberElement.Value < rule.ValueNumber,
+                        ComparisonOperator.GreaterThanOrEqual => numberElement.Value >= rule.ValueNumber,
+                        ComparisonOperator.LessThanOrEqual => numberElement.Value <= rule.ValueNumber,
+                        _ => false,
+                    };
+
+                    checkedRules.Add((rule.LogicalOperator, result));
+                }
+                else if(element is FormElementWithOptions optionsElement)
+                {
+                    bool result = rule.ComparisonOperator switch
+                    {
+                        ComparisonOperator.Equals => optionsElement.Value == rule.ValueString,
+                        ComparisonOperator.NotEquals => optionsElement.Value != rule.ValueString,
+                        _ => false,
+                    };
+
+                    checkedRules.Add((rule.LogicalOperator, result));
+                }
+                else if(element is FormDateElement dateElement)
+                {
+                    bool result = rule.ComparisonOperator switch
+                    {
+                        ComparisonOperator.Equals => dateElement.Value.Date == rule.ValueDate.Date,
+                        ComparisonOperator.NotEquals => dateElement.Value.Date != rule.ValueDate.Date,
+                        ComparisonOperator.GreaterThan => dateElement.Value.Date > rule.ValueDate.Date,
+                        ComparisonOperator.LessThan => dateElement.Value.Date < rule.ValueDate.Date,
+                        ComparisonOperator.GreaterThanOrEqual => dateElement.Value.Date >= rule.ValueDate.Date,
+                        ComparisonOperator.LessThanOrEqual => dateElement.Value.Date <= rule.ValueDate.Date,
+                        _ => false,
+                    };
+
+                    checkedRules.Add((rule.LogicalOperator, result));
+                }
+                else if(element is FormCheckboxElement checkboxElement)
+                {
+                    bool result = rule.ComparisonOperator switch
+                    {
+                        ComparisonOperator.Equals => checkboxElement.Value == rule.ValueBoolean,
+                        ComparisonOperator.NotEquals => checkboxElement.Value != rule.ValueBoolean,
+                        _ => false,
+                    };
+
+                    checkedRules.Add((rule.LogicalOperator, result));
+                }
+            }
+
+            // When someone has manipulated the values within the database then me might not have any results at all.
+            if(!checkedRules.Any())
+            {
+                return false;
+            }
+
+            bool returnResult = checkedRules.First().result;
+
+            for (int i = 1; i < checkedRules.Count; i++)
+            {
+                (LogicalOperator logicOperator, bool result) tmp = checkedRules[i];
+
+                returnResult = tmp.logicOperator switch
+                {
+                    LogicalOperator.And => returnResult && tmp.result,
+                    LogicalOperator.Or => returnResult || tmp.result,
+                    _ => false,
+                };
+            }
+            
+
+            return returnResult;
+        }
+
+        
     }
 }
