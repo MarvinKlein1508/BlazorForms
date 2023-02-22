5b879bc05d52f5462be2302c7daf016550a2fa1f
Marvin Klein <marvin.klein@harold-scholz.de>
2023-02-22 08:52:30 +0100

    Validate FormNumberElement

--- FormPortal.Core.Validators/Admin/FormNumberElementValidator.cs
+++ FormPortal.Core.Validators/Admin/FormNumberElementValidator.cs
@@ -13,6 +13,35 @@ namespace FormPortal.Core.Validators.Admin
 
             RuleFor(x => x.DecimalPlaces)
                 .GreaterThanOrEqualTo(0);
+
+
+            RuleFor(x => x.Value)
+                .GreaterThanOrEqualTo(x => x.MinValue)
+                .When(IsEntryMode);
+
+            RuleFor(x => x.Value)
+                .LessThanOrEqualTo(x => x.MaxValue)
+                .When((x) => x.MaxValue > 0 && IsEntryMode(x));
+
+            RuleFor(x => x.Value)
+                .Must(IsNumberSet)
+                .When(x => IsValueRequired(x) && IsEntryMode(x));
+        }
+
+        private bool IsNumberSet(FormNumberElement element, decimal number)
+        {
+            bool result = true;
+            if(element.MinValue != 0)
+            {
+                result = result && number >= element.MinValue;
+            }
+
+            if(element.MaxValue != 0)
+            {
+                result = result && number <= element.MaxValue;
+            }
+
+            return result;
         }
     }
 
