eb1eb2df9f6a0ba9978a0e21f528822a36a5d066
Marvin Klein <marvin.klein@harold-scholz.de>
2023-02-23 12:46:25 +0100

    Implement FormFileElementValidator

--- FormPortal.Core.Validators/Admin/FormFileElementValidator.cs
+++ FormPortal.Core.Validators/Admin/FormFileElementValidator.cs
@@ -1,10 +1,27 @@
-﻿using FormPortal.Core.Models.FormElements;
+﻿using FluentValidation;
+using FluentValidation.Results;
+using FormPortal.Core.Models.FormElements;
 
 namespace FormPortal.Core.Validators.Admin
 {
     public class FormFileElementValidator : FormElementValidator<FormFileElement>
     {
+        public FormFileElementValidator() : base()
+        {
+            RuleFor(x => x.Value)
+                .Custom(ValidateValue)
+                .When(IsEntryMode);
+        }
 
+        public void ValidateValue(byte[] value, ValidationContext<FormFileElement> context)
+        {
+            var element = context.InstanceToValidate;
+
+            if (IsValueRequired(element) && value.Length > 0)
+            {
+                context.AddFailure(new ValidationFailure(context.PropertyName, $"{element.Name} erfordert eine Datei."));
+            }
+        }
     }
 
 
