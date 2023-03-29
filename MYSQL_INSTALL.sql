69211755a3611e358d194d115ca6a77573343579
Marvin Klein <marvin.klein@harold-scholz.de>
2023-03-29 09:14:07 +0200

    Use foreign key

--- MYSQL_INSTALL.sql
+++ MYSQL_INSTALL.sql
@@ -25,7 +25,7 @@ CREATE TABLE user_permissions
 	user_id INTEGER NOT NULL,
 	permission_id INTEGER NOT NULL,
 	PRIMARY KEY(user_id, permission_id),
-	FOREIGN KEY (user_id) REFERENCES users(user_id),
+	FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
 	FOREIGN KEY (permission_id) REFERENCES permissions(permission_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
