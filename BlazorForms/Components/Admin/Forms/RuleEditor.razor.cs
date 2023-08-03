d7a88fff0b497e1dbc3ae98a6930274c6baa62c3
MarvinKlein1508 <support@marvinkleinmusic.de>
2023-08-03 09:46:37 +0200

    Provide translations for RuleEditor

---
+++ BlazorForms/Components/Admin/Forms/RuleEditor.razor.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorForms.Components.Admin.Forms
+{
+    /// <summary>
+    /// This non generic class only exists for localization support
+    /// </summary>
+    public partial class RuleEditor
+    {
+    }
+}
