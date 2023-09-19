64d367f1981a3f8baa2a8c9b2bd4dca760e2fcb2
MarvinKlein1508 <support@marvinkleinmusic.de>
2023-09-19 11:34:35 +0200

    Add localization for FormNumberElement

--- BlazorForms.Core/Validators/Admin/FormNumberElementValidator.cs
+++ BlazorForms.Core/Validators/Admin/FormNumberElementValidator.cs
@@ -11,7 +11,7 @@ namespace BlazorForms.Core.Validators.Admin
         {
             RuleFor(x => x.MinValue)
                 .Must((x, y) => x.MaxValue >= x.MinValue)
-                .WithMessage("Der Mindestwert kann nicht größer sein, als der Maximalwert.")
+                .WithMessage(_localizer["VALIDATION_MIN_MAX"])
                 .When(x => x.MaxValue != 0);
 
             RuleFor(x => x.DecimalPlaces)
@@ -28,15 +28,15 @@ namespace BlazorForms.Core.Validators.Admin
             FormNumberElement element = context.InstanceToValidate;
             if (IsValueRequired(element) && number is 0)
             {
-                context.AddFailure(new ValidationFailure(context.PropertyPath, $"{element.Name} darf nicht 0 sein."));
+                context.AddFailure(new ValidationFailure(context.PropertyPath, String.Format(_localizer["VALIDATION_REQUIRED"], element.Name)));
             }
             else if (element.MinValue > 0 && number < element.MinValue)
             {
-                context.AddFailure(new ValidationFailure(context.PropertyPath, $"{element.Name} muss mindestens den Wert {element.MinValue} betragen."));
+                context.AddFailure(new ValidationFailure(context.PropertyPath, String.Format(_localizer["VALIDATION_LESS_THAN_MIN_VALUE"], element.Name, element.MinValue)));
             }
             else if (element.MaxValue > 0 && number > element.MaxValue)
             {
-                context.AddFailure(new ValidationFailure(context.PropertyPath, $"{element.Name} kann maximal den Wert {element.MaxValue} betragen."));
+                context.AddFailure(new ValidationFailure(context.PropertyPath, String.Format(_localizer["VALIDATION_GREATER_THAN_MAX_VALUE"], element.Name, element.MaxValue)));
             }
         }
     }
