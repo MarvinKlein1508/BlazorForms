fbd454881466ca03d8bc734891bc0c9627a1f2bd
MarvinKlein1508 <support@marvinkleinmusic.de>
2023-09-19 16:46:33 +0200

    Remove legacy columns

--- BlazorForms.Core/Models/FormEntryStatusChange.cs
+++ BlazorForms.Core/Models/FormEntryStatusChange.cs
@@ -16,13 +16,7 @@ namespace BlazorForms.Core.Models
         public string Comment { get; set; } = string.Empty;
         [CompareField("date_added")]
         public DateTime DateAdded { get; set; }
-        [CompareField("notify_creator")]
-        public bool NotifyCreator { get; set; }
-        [CompareField("notify_managers")]
-        public bool NotifyManagers { get; set; }
-        [CompareField("notify_approvers")]
-        public bool NotifyApprovers { get; set; }
-
+        
         public List<FormEntryHistoryNotify> Notifiers { get; set; } = new();
 
         [CompareField("display_name")]
@@ -37,10 +31,7 @@ namespace BlazorForms.Core.Models
                 { "STATUS_ID", StatusId },
                 { "USER_ID", UserId },
                 { "COMMENT", Comment },
-                { "DATE_ADDED", DateAdded },
-                { "NOTIFY_CREATOR", NotifyCreator },
-                { "NOTIFY_MANAGERS", NotifyManagers },
-                { "NOTIFY_APPROVERS", NotifyApprovers }
+                { "DATE_ADDED", DateAdded }
             };
         }
     }
