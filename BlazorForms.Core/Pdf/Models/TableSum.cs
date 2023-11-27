1d1acb297ff3572fa189a4e498e63055db741158
MarvinKlein1508 <support@marvinkleinmusic.de>
2023-11-27 15:04:54 +0100

    First draft

--- BlazorForms.Core/Pdf/Models/TableSum.cs
+++ BlazorForms.Core/Pdf/Models/TableSum.cs
@@ -2,12 +2,20 @@
 {
     public class TableSum
     {
+        public string Name { get; set; } = string.Empty;
         public string NumberFormat { get; }
         public decimal Value { get; set; }
         public TableSum(string numberFormat, decimal value)
         {
             NumberFormat = numberFormat;
             Value = value;
+        }
+
+        public TableSum(string numberFormat, decimal value, string name)
+        {
+            NumberFormat = numberFormat;
+            Value = value;
+            Name = name;
         }
 
     }
