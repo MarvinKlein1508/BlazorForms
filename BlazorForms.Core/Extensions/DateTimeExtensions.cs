d74aefc59734a4e495d9af224fd6b42cd8a6eb8e
MarvinKlein1508 <support@marvinkleinmusic.de>
2024-01-12 11:34:13 +0100

    Localize GetElapsedTimeString

--- BlazorForms.Core/Extensions/DateTimeExtensions.cs
+++ BlazorForms.Core/Extensions/DateTimeExtensions.cs
@@ -1,15 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Resources;
 
 namespace BlazorForms.Core.Extensions
 {
     public static class DateTimeExtensions
     {
-        public static string RelativeZeit(this DateTime yourDate)
+        private static readonly ResourceManager _ressourceManager = new ResourceManager("BlazorForms.Core.Languages.Extensions.DateTimeExtensions", typeof(DateTimeExtensions).Assembly);
+        public static string GetElapsedTimeString(this DateTime yourDate)
         {
+
             const int SECOND = 1;
             const int MINUTE = 60 * SECOND;
             const int HOUR = 60 * MINUTE;
@@ -19,35 +17,50 @@ namespace BlazorForms.Core.Extensions
 
             if (delta < 1 * MINUTE)
             {
-                return ts.Seconds == 1 ? "vor einer Sekunde" : $"vor {ts.Seconds} Sekunden";
+                return GetLocalizedMessage(ts.Seconds, "SECONDS_AGO");
             }
 
             if (delta < 2 * MINUTE)
             {
-                return "vor einer Minute";
+                return GetLocalizedMessage("MINUTE_AGO");
             }
 
             if (delta < 45 * MINUTE)
             {
-                return $"vor {ts.Minutes} Minuten";
+                return GetLocalizedMessage(ts.Minutes, "X_MINUTES_AGO");
             }
 
             if (delta < 120 * MINUTE)
             {
-                return "vor einer Stunde";
+                return GetLocalizedMessage("HOUR_AGO");
             }
 
             if (delta < 24 * HOUR)
             {
-                return $"vor {ts.Hours} Stunden";
+                return GetLocalizedMessage(ts.Hours, "X_HOURS_AGO");
             }
 
             if (delta < 48 * HOUR)
             {
-                return "gestern";
+                return GetLocalizedMessage("YESTERDAY");
             }
 
-            return DateTime.Now.ToString("dd. MMMM yyyy");
+            // Fallback to a default date format if none of the conditions are met
+            return yourDate.ToString(GetLocalizedMessage("DEFAULT_DATE_FORMAT"));
+        }
+
+        private static string GetLocalizedMessage(string key)
+        {
+            return _ressourceManager.GetString(key) ?? key;
+        }
+
+        private static string GetLocalizedMessage(int count, string key)
+        {
+            var message = GetLocalizedMessage(key);
+
+            return String.Format(message, count);
         }
     }
+
+
 }
