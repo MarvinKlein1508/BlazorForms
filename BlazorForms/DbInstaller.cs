2f42e283ddadf0ecf073bed6adb4641db2a56210
MarvinKlein1508 <support@marvinkleinmusic.de>
2024-01-15 12:05:41 +0100

    Update trigger

--- BlazorForms/DbInstaller.cs
+++ BlazorForms/DbInstaller.cs
@@ -726,11 +726,15 @@ namespace BlazorForms
         {
             yield return
                 """
-                CREATE TRIGGER update_status_id
+                CREATE OR REPLACE TRIGGER update_status_id
                     AFTER INSERT
                     ON form_entry_history FOR EACH ROW
                 BEGIN
-                    UPDATE form_entries SET status_id = new.status_id WHERE entry_id = new.entry_id;
+                    UPDATE form_entries SET 
+                        status_id = new.status_id,
+                        last_change_user_id = new.user_id,
+                        last_change = CURRENT_TIMESTAMP
+                    WHERE entry_id = new.entry_id;
                 END;   
                 """;
         }
