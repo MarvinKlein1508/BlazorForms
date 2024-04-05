6153befaedeec0250f7b2cea56f6a9dd662cee2e
MarvinKlein1508 <support@marvinkleinmusic.de>
2024-04-05 12:57:33 +0200

    Add FormEntryManagerContentValidator

---
+++ BlazorForms.Core/Validators/FormEntryManagerContentValidator.cs
@@ -0,0 +1,13 @@
+﻿using BlazorForms.Core.Models;
+using FluentValidation;
+
+namespace BlazorForms.Core.Validators;
+
+public class FormEntryManagerContentValidator : AbstractValidator<FormEntryManagerContent>
+{
+    public FormEntryManagerContentValidator()
+    {
+        RuleFor(x => x.Note)
+            .MaximumLength(5000);
+    }
+}
