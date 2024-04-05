be472b3e4a1c7bd2e460d0000b116ccf6eedeb8c
MarvinKlein1508 <support@marvinkleinmusic.de>
2024-04-05 12:56:20 +0200

    Add FormEntryManagerContent model

---
+++ BlazorForms.Core/Models/FormEntryManagerContent.cs
@@ -0,0 +1,23 @@
+﻿using DbController;
+
+namespace BlazorForms.Core.Models;
+
+public class FormEntryManagerContent : IDbParameterizable
+{
+    [CompareField("entry_id")]
+    public int EntryId { get; set; }
+    [CompareField("work_user_id")]
+    public int? WorkUserId { get; set; } = null;
+    [CompareField("note")]
+    public string Note { get; set; } = string.Empty;
+
+    public Dictionary<string, object?> GetParameters()
+    {
+        return new Dictionary<string, object?>
+        {
+            { "ENTRY_ID", EntryId },
+            { "WORK_USER_ID", WorkUserId },
+            { "NOTE", Note },
+        };
+    }
+}
