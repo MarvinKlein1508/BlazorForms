64605dc6f4663bbcf772c110418e757cf65b453f
MarvinKlein1508 <support@marvinkleinmusic.de>
2024-04-25 10:42:53 +0200

    Added area

--- BlazorForms/Components/FormEntryManagerArea.razor.cs
+++ BlazorForms/Components/FormEntryManagerArea.razor.cs
@@ -6,6 +6,18 @@ namespace BlazorForms.Components
     public partial class FormEntryManagerArea
     {
         [Parameter, EditorRequired]
-        public FormEntry? Entry { get; set; } 
+        public FormEntry? Entry { get; set; }
+
+        public FormEntryManagerContent Input { get; set; } = new();
+        protected override Task OnParametersSetAsync()
+        {
+
+            if (Entry is not null)
+            {
+
+            }
+
+            return base.OnParametersSetAsync();
+        }
     }
 }
