f2e7aca1d238691815abeb9b81a45f84814b1bcf
MarvinKlein1508 <support@marvinkleinmusic.de>
2024-07-26 10:53:24 +0200

    Add dbup .net 8

---
+++ dbup-mysql/MySqlObjectParser.cs
@@ -0,0 +1,14 @@
+﻿using DbUp.Support;
+
+namespace DbUp.MySql
+{
+    /// <summary>
+    /// Parses Sql Objects and performs quoting functions
+    /// </summary>
+    public class MySqlObjectParser : SqlObjectParser
+    {
+        public MySqlObjectParser() : base("`", "`")
+        {
+        }
+    }
+}
