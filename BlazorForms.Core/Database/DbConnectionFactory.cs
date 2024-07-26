1431777c990cb97ff6cdf5dc8307abc75ff56989
MarvinKlein1508 <support@marvinkleinmusic.de>
2024-07-26 08:58:56 +0200

    Add DbConnectionFactory

---
+++ BlazorForms.Core/Database/DbConnectionFactory.cs
@@ -0,0 +1,26 @@
+﻿using MySqlConnector;
+using System.Data;
+
+namespace BlazorForms.Core.Database;
+
+public interface IDbConnectionFactory
+{
+    Task<IDbConnection> CreateConnectionAsync(CancellationToken token = default);
+}
+
+
+public class MySqlConnectionFactory : IDbConnectionFactory
+{
+    private readonly string _connectionString;
+
+    public MySqlConnectionFactory(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+    public async Task<IDbConnection> CreateConnectionAsync(CancellationToken token = default)
+    {
+        var connection = new MySqlConnection(_connectionString);
+        await connection.OpenAsync(token);
+        return connection;
+    }
+}
