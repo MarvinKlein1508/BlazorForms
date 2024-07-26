9becbe742cf424ea9478ca85a1922a5302e1d81f
MarvinKlein1508 <support@marvinkleinmusic.de>
2024-07-26 09:00:42 +0200

    Add DbInitializer

---
+++ BlazorForms.Core/Database/DbInitializer.cs
@@ -0,0 +1,35 @@
+﻿using DbUp;
+
+namespace BlazorForms.Core.Database;
+
+public class DbInitializer
+{
+    private readonly string _connectionString;
+
+    public DbInitializer(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public Task<int> InitializeAsync()
+    {
+        EnsureDatabase.For.MySqlDatabase(_connectionString);
+
+        var upgrader = DeployChanges.To.MySqlDatabase(_connectionString)
+            .WithScriptsEmbeddedInAssembly(typeof(DbInitializer).Assembly)
+            .LogToConsole()
+            .Build();
+
+        if (upgrader.IsUpgradeRequired())
+        {
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                return Task.FromResult(-1);
+            }
+        }
+
+        return Task.FromResult(0);
+    }
+}
