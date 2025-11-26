381e09be66d3b6bbee2857244ab5271615f7d108
MarvinKlein1508 <support@marvinkleinmusic.de>
2025-11-26 14:39:24 +0100

    More fixes

--- src/BlazorForms.Web/Components/FluentConstants.cs
+++ src/BlazorForms.Web/Components/FluentConstants.cs
@@ -1,6 +1,9 @@
-﻿namespace BlazorForms.Web.Components;
+namespace BlazorForms.Web.Components;
 
 public static class FluentConstants
 {
     public const string MESSAGEBAR_TOP_SECTION = "MESSAGES_TOP";
+
+    public const string FLUENTSTACK_VERTICAL_GAP = "10px";
+    public const string FLUENTSTACK_HORIZONTAL_GAP = "10px";
 }
